0c8319656eceafdd72a1090b564fc94ff067da92
Jeeban <jeebanbashyal1234@gmail.com>
2023-12-27 14:50:33 +0545

    First day commit

---
+++ day1.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator.");
+city = input("What is the name of the city you grew up?");
+pet = input("What is your pet name?");
+
+print("Your band name could be " + city + " "+ pet );
