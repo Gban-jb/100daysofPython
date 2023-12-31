a26e06b18c273e6fa77acd957213f746850cb1e8
Jeeban <jeebanbashyal1234@gmail.com>
2023-12-31 14:45:38 +0545

    Day-3 Odd Even Number

---
+++ day3.py/start.py
@@ -0,0 +1,8 @@
+print("Program of finding ODD or Even Number.");
+num = int(input("Enter any number you want: "));
+if num % 2 == 0:
+ print("It's a Even Number");
+else:
+    print("This number is odd");
+    
+    
