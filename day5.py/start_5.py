fc3ba3ec07a1d0db023920aef9b95784ab5fde08
Jeeban <jeebanbashyal1234@gmail.com>
2024-01-02 13:21:42 +0545

    For loop

---
+++ day5.py/start_5.py
@@ -0,0 +1,9 @@
+print("Loop and indentation Example")
+
+fruits = ["Apple", "Banana", "Cucumber", "Dragons Fruit", "Guava", "Mango"]
+for fruit in fruits:
+    print(fruit)
+    print(fruit + " Sweet")
+print(fruits)
+
+    
