e7ac7a673fa0aaf312b42fce41ee177e5d0ce83a
Jeeban <jeebanbashyal1234@gmail.com>
2024-01-09 13:43:27 +0545

    Day8 start

--- Day8.py/one.py
+++ Day8.py/one.py
@@ -1,1 +1,11 @@
 print("This is Day8. And welcome to you!")
+
+
+def greet(name, location):
+    print(f"Hello {name}")
+    print(f"You are at the {location}.")
+    
+greet("Jeeban", "Kathmandu")
+
+greet( location="Nepal", name="Brayn")
+
