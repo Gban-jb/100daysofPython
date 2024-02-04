4097202bd183ca8bd3b45e7564813f412c0e1a28
Jeeban <jeebanbashyal1234@gmail.com>
2024-02-04 12:39:31 +0545

    Day9 started

---
+++ Day9.py/dictionary.py
@@ -0,0 +1,15 @@
+this_dict = {
+  "name" : "Jeeban",
+  "Age" : 20,
+  "DOB" : 2003,
+  "Address" : "Paiyun, Nepal",
+  "Hobby" : ["Playing Football", " CHess", "travelling", "Coding"],
+  "Stock_market" : True
+  }
+ 
+this_dict["Last_name"] = "Bashyal"
+
+print(this_dict)
+
+for thing in this_dict:
+  print(thing)
